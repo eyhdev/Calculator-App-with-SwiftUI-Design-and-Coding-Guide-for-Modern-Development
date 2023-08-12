62df72cc73464f318e30f38ecae973e0d871751d
eyhdev <alaqsa335@gmail.com>
2023-08-12 16:05:21 +0300

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by eyh.mac on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
